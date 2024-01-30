40a30bba83979dcae5f02de0e2f1b62789ef7748
Zhenya Hitrova <jarotea@yandex.ru>
2024-01-30 23:11:54 +1000

    Add card is ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
 
+const createNewCard = function (card, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  deleteButton.addEventListener("click", deleteCard);
+
+  placesList.append(cardElement);
+};
+
 // @todo: Функция удаления карточки
+const deleteCard = function (card) {
+  return placesList.remove(card);
+};
 
 // @todo: Вывести карточки на страницу
+const addCards = function () {
+  for (let i = 0; i < initialCards.length; i++) {
+    let card = initialCards[i];
+    createNewCard(card);
+  }
+};
+
+addCards();
