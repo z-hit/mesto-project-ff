eef23084c884ed9c3c02ca7c83561dc77610cc3b
Zhenya Hitrova <jarotea@yandex.ru>
2024-02-18 23:26:01 +1000

    Webpack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/z-hit/mesto-project-ff#readme
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "../pages/index.css";
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
