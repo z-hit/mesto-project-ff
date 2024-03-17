49ebdc761ebdb3d13e1d5a1e94831ca96fcc4866
Zhenya Hitrova <jarotea@yandex.ru>
2024-03-17 21:37:12 +1000

    Api file refined

--- src/components/api.js
+++ src/components/api.js
@@ -6,81 +6,61 @@ const apiConfig = {
   },
 };
 
-function handlePromiseResolve(response) {
-  if (response.ok) {
-    return response.json();
-  }
-  return Promise.reject(`Ошибка: ${response.status}`);
-}
-
-function getUserData() {
-  return fetch(`${apiConfig.baseURL}users/me`, {
-    method: "GET",
-    headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
-}
-
-function getInitialCards() {
-  return fetch(`${apiConfig.baseURL}cards`, {
-    method: "GET",
-    headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
-}
-
-//abstract func test
-
-/* function workWithServer(url, method) {
+function fetchServer(url, method, body) {
   return fetch(url, {
     method: method,
     headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
-} */
+    body: body,
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(`Ошибка: ${response.status}`);
+  });
+}
 
-// test over
+function getUserData() {
+  return fetchServer(`${apiConfig.baseURL}users/me`, "GET");
+}
+
+function getInitialCards() {
+  return fetchServer(`${apiConfig.baseURL}cards`, "GET");
+}
 
 function addNewCardToServer(newCardData) {
-  return fetch(`${apiConfig.baseURL}cards`, {
-    method: "POST",
-    headers: apiConfig.headers,
-    body: JSON.stringify(newCardData),
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(
+    `${apiConfig.baseURL}cards`,
+    "POST",
+    JSON.stringify(newCardData)
+  );
 }
 
 function patchProfileInfo(newUserData) {
-  return fetch(`${apiConfig.baseURL}users/me`, {
-    method: "PATCH",
-    headers: apiConfig.headers,
-    body: JSON.stringify(newUserData),
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(
+    `${apiConfig.baseURL}users/me`,
+    "PATCH",
+    JSON.stringify(newUserData)
+  );
 }
 
 function patchAvatar(newAvatarLink) {
-  return fetch(`${apiConfig.baseURL}users/me/avatar`, {
-    method: "PATCH",
-    headers: apiConfig.headers,
-    body: JSON.stringify(newAvatarLink),
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(
+    `${apiConfig.baseURL}users/me/avatar`,
+    "PATCH",
+    JSON.stringify(newAvatarLink)
+  );
 }
 
 function deleteCardFromServer(cardToDelete) {
-  return fetch(`${apiConfig.baseURL}cards/${cardToDelete.id}`, {
-    method: "DELETE",
-    headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(`${apiConfig.baseURL}cards/${cardToDelete.id}`, "DELETE");
 }
 
 function putLikeToServer(cardID) {
-  return fetch(`${apiConfig.baseURL}cards/likes/${cardID}`, {
-    method: "PUT",
-    headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(`${apiConfig.baseURL}cards/likes/${cardID}`, "PUT");
 }
 
 function removeLikeFromServer(cardID) {
-  return fetch(`${apiConfig.baseURL}cards/likes/${cardID}`, {
-    method: "DELETE",
-    headers: apiConfig.headers,
-  }).then((res) => handlePromiseResolve(res));
+  return fetchServer(`${apiConfig.baseURL}cards/likes/${cardID}`, "DELETE");
 }
 
 export {
